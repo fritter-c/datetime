d418a55b5bf09b9e75a981dc412af2d3d8eced81
Fernando Lahude Ritter <CIANDT\fernandoritter@lnb029507cps.ciandt.global>
2024-07-25 13:05:02 -0300

    refactor

--- datetime.cpp
+++ datetime.cpp
@@ -1,111 +1,6 @@
 #include "datetime.h"
 
 namespace gtr {
-    
-/**
- * Months abbreviation
- */
-    constexpr const char *datetime_month_abbrev[]{"Jan", "Feb", "Mar", "Apr",
-                                                  "May", "Jun", "Jul", "Aug",
-                                                  "Sep", "Oct", "Nov", "Dec"};
-
-    constexpr inline static bool
-    is_alpha(const char c){return (c >= '0' && c <= '9');}
-
-/**
-* For fasting parsing.
-* @param sum of chars in the month abbreviation
-* @return return the month 1-indexed
-* @note: NOT SAFE for uncontrolled inputs
-*/
-    constexpr inline static
-    int get_month_from_sum(int sum){
-        constexpr const int char_sum[] = {
-            281,  // Jan
-            269,  // Feb
-            288,  // Mar
-            291,  // Apr
-            295,  // May
-            301,  // Jun
-            299,  // Jul
-            401,  // Aug
-            296,  // Sep
-            294,  // Oct
-            307,  // Nov
-            268   // Dec
-        };
-        int month = 1;
-        while(char_sum[month - 1] != sum) month++;
-        return month;
-    }
-    
-/**
- * Get the numbers of digits for a given year (-999999 to 999999)
- * @param n The year
- * @return The number of digits or -1 if mod(n) > 999999
- */
-    constexpr inline static int
-    datetime_year_digits(int n){
-        if (n < 0) n = -n;
-        if (n < 10) return 1;
-        if (n < 100) return 2;
-        if (n < 1000) return 3;
-        if (n < 10000) return 4;
-        if (n < 100000) return 5;
-        if (n < 1000000) return 6;
-        // Not suported in microseconds precision
-        return -1;
-    }
-
-/**
- * Positive atoi given dates are mostly positive
- * @param number The number string
- * @return number as an positive unsigned int
- */
-    static inline constexpr unsigned int
-    positive_atoi(const char* number){
-        unsigned int result  = 0;
-        while(*number != '\0') result = result * 10 + *number++ - '0';
-        return result;
-    }
-
-/*
- * strcpy implementation
- * @param dest Destination string
- * @param data Source string
- */
-    inline static void
-    string_copy(char* dest, const char* data){
-        do{*dest++ = *data++;}while(*data != '\0');
-    }
-
-/**
- * Puts a positive integer of n digits in the buffer plus the null-terminator
- * @param dest Destination string
- * @param digits Number of digits
- * @param number Integer positive number
- * @attention dest should support 4 + 1 char
- */
-    static inline void
-    put_positive_number(char *dest,const unsigned int digits,int number) {
-        *(dest += digits) = '\0'; 
-        for(unsigned int i= 0; i < digits; i++) {*--dest = (number % 10) + '0'; number /= 10;}
-    }
-
-/**
- * Puts a negative sign plus the negative integer of n digits in the buffer plus the null-terminator
- * @param dest Destination string
- * @param digits Number of digits
- * @param number Integer negative number
- * @attention dest should support 4 + 2 char
- */
-    static inline void
-    put_negative_number(char * dest, const unsigned int digits, int number){
-        *(dest+= digits +1) = '\0';
-        number = -number;
-        for(unsigned int i= 0; i < digits; i++) {*--dest = (number % 10) + '0'; number /= 10;}
-        *--dest = '-';
-    }
     constexpr unsigned int g_monthdays[13] = {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365};
     constexpr unsigned int g_lpmonthdays[13] = {0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366};
 
@@ -120,8 +15,8 @@ namespace gtr {
                         const int year, const int hour,
                         const int minute, const int second) {
 
-        //  Reference https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap04.html#tag_04
-        unsigned int day_corrected = days_until_month(year, month) + day - 1;
+        // Reference https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap04.html#tag_04
+        unsigned int day_corrected = days_until_month(year, month > 0 && month <= 12 ? month : 12) + day - 1;
         unsigned int year_corrected = year - 1900;
         return second + minute * 60 + hour * 3600 + day_corrected * 86400 +
             (year_corrected - 70) * 31536000 +
@@ -205,210 +100,56 @@ namespace gtr {
  * @return True if the conversion was successful, false otherwise.
  */
     static bool
-    datetime_to_string(int year, int month, int day, int hour,
-                       int minute, int second,
-                       int microsecond, char* out, char* error,
+    datetime_to_string(datetime date, char* out,
                        const char *format = DATETIME_DEFAULT_FORMAT, date_format group_format = date_format::text_date) {
         if (group_format == date_format::text_date){
+            datetime_pack pack;
+            date.to_pack(pack);
             const char* state = format;
             char* out_ptr = out;
-            string_copy(error, "");
             while (*state != '\0') {
                 switch (*state) {
-                case 'D':
-                    if (*(state + 1) == 'D') {
-                        put_positive_number(out_ptr, 2, day);
-                        state += 2;
-                    } else {
-                        string_copy(error, "Invalid day format (two digits required)"); 
-                        *out_ptr = '\0';
-                        return false;
-                    }
-                    out_ptr += 2;
-                    break;
+                case 'D':day_field::puts(&state, &out_ptr, pack); break;
                 case 'M':
-                    if (*(state + 1) == 'M' && *(state + 2) == 'M'){
-                        string_copy(out_ptr, datetime_month_abbrev[month -1]);
-                        state+= 3;
-                        out_ptr += 3;
-                    }   
-                    else if (*(state + 1) == 'M') {
-                        put_positive_number(out_ptr, 2, month);
-                        state += 2;
-                        out_ptr += 2;
-                    }                  
-                    else {
-                        string_copy(error,"Invalid month format (two digits required)");
-                        *out_ptr = '\0';
-                        return false;
-                    }
+                    if(*(state + 1) == 'M' && *(state +2) == 'M')month_field<month_format::month_abbrev>::puts(&state,&out_ptr,pack);
+                    else month_field<>::puts(&state,&out_ptr,pack);
                     break;
-                case 'Y':
-                    // Puts only 4 signed digits years (useful from 9999BC to 9999AC)
-                    if (*(state + 1) == 'Y' && *(state + 2) == 'Y' && *(state + 3) == 'Y') {
-                        if (year >= 0)[[likely]]{
-                            put_positive_number(out_ptr, 4, year);
-                            out_ptr += 4;
-                        }
-                        else{
-                            put_negative_number(out_ptr, 4, year);
-                            out_ptr += 5;
-                        }
-                        state += 4;            
-                    }
-                    //Puts only 2 signed digits
-                    else if (*(state + 1) == 'Y'){
-                        int year_format = year % 100;
-                        if (year_format >= 0)[[likely]]{
-                            put_positive_number(out_ptr, 2, year_format);
-                            out_ptr += 2;
-                        }
-                        else{
-                            put_negative_number(out_ptr, 2, year_format);
-                            out_ptr += 3;
-                        }
-                        state += 2;
-                    }
-                    // Puts the whole signed number
-                    else if (*(state + 1) == 'F'){
-                        int digits = datetime_year_digits(year);
-                        if (year > 0) [[likely]]
-                            put_positive_number(out_ptr, digits, year);
-                        else
-                            put_negative_number(out_ptr, digits++, year);
-                        state += 2;
-                        out_ptr += digits;
-                    }
-                    else {
-                        string_copy(error,"Invalid year format (four digits required)");
-                        *out_ptr = '\0';
-                        return false;
-                    }
-                    break;
-                case 'h':
-                    if (*(state + 1) == 'h') {
-                        put_positive_number(out_ptr, 2, hour);
-                        state += 2;
-                    } else {
-                        string_copy(error,"Invalid hour format (two digits required)");
-                        *out_ptr = '\0';
-                        return false;
-                    }
-                    out_ptr += 2;
-                    break;
-                case 'm':
-                    if (*(state + 1) == 'm') {
-                        put_positive_number(out_ptr, 2, minute);
-                        state += 2;
-                    } else {
-                        string_copy(error,"Invalid minute format (two digits required)");
-                        *out_ptr = '\0';
-                        return false;
-                    }
-                    out_ptr += 2;
-                    break;
-                case 's':
-                    if (*(state + 1) == 's') {
-                        put_positive_number(out_ptr, 2, second);
-                        state += 2;
-                    } else {
-                        string_copy(error, "Invalid second format (two digits required)");
-                        *out_ptr = '\0';
-                        return false; 
-                    }
-                    out_ptr += 2;
-                    break;
-                case 'z':
-                {
-                    int digits = 1;
-                    while(*++state == 'z') {digits++;}
-                    put_positive_number(out_ptr, digits, microsecond);
-                    out_ptr += digits;
-                    break; 
-                }
-                default:                
-                    *out_ptr++ = *state++;
-                    break;
+                case 'Y':year_field::puts(&state, &out_ptr, pack);break;
+                case 'h':hour_field::puts(&state, &out_ptr, pack);break;
+                case 'm':minute_field::puts(&state, &out_ptr, pack);break;
+                case 's':second_field::puts(&state,&out_ptr, pack);break;
+                case 'z':microsecond_field::puts(&state, &out_ptr, pack);break;
+                default:separator_field<1>::puts(&state, &out_ptr, pack);break;
                 }
             }
             *out_ptr = '\0';
             return true;
         }
-        return datetime_to_string(year, month, day, hour, minute, second, microsecond, out, error, "YYYY-MM-DDThh:mm:ss+00:00", date_format::text_date);
+        return datetime_to_string(date, out, "YYYY-MM-DDThh:mm:ss+00:00", date_format::text_date);
     }
 
     static long long
     parse_datetime_string(const char * __restrict date, const char * __restrict format, date_format group_format = date_format::text_date) {
-        int year{0}, month{0}, day{0}, hour{0}, minute{0}, second{0}, microsecond{0};
         const char* state = format;
         const char* date_char = date;
+        datetime_pack pack{};
         if (group_format == date_format::text_date){
             while(*state != '\0'){
-                char buffer[8];
-                unsigned int index = 0;
-                char init_state = *state;
-                int* result = &day;
-                bool separator = false;
-                bool special = false;
                 switch(*state){
                 case 'M':
-                    if (*(state +1) == 'M' && *(state + 2 ) == 'M'){
-                        special = true;
-                        int sum = *date_char + *(date_char + 1) + *(date_char + 2);
-                        month = get_month_from_sum(sum);
-                        state += 3;
-                        date_char += 3;
-                    }
-                    result = &month;
+                    if (*(state +1) == 'M' && *(state + 2 ) == 'M')state += month_field<month_format::month_abbrev>::parse(&date_char, pack);
+                    else state += month_field<>::parse(&date_char, pack);
                     break;
-                case 'Y':
-                    if (*(state + 1) == 'F'){
-                        special = true;
-                        while(is_alpha(*date_char))
-                            buffer[index++] = *date_char++;
-                        buffer[index] = '\0';
-                        year = positive_atoi(buffer);
-                    }
-                    result = &year;
-                    break;
-                case 'D':
-                    result = &day;
-                    break;
-                case 'h':
-                    result = &hour;
-                    break;
-                case 'm':
-                    result = &minute;
-                    break;
-                case 's':
-                    result = &second;
-                    break;
-                case 'z':
-                    result = &microsecond;
-                    break;
-                default:
-                    separator = true;
-                    break;
-                }
-                if (!separator && !special){
-                    while(*state == init_state && *date_char != '\0')
-                    {
-                        buffer[index++] = *date_char++;
-                        state++;
-                    }
-                    //Fill with zeros the rest in case no all 6 digits were provided
-                    if (init_state == 'z')
-                        while(index < 6) buffer[index++] = '0';
-                                      
-                    buffer[index] = '\0';
-                    *result = positive_atoi(buffer);
-                }
-                else{
-                    state++;
-                    date_char++;
+                case 'Y':state += year_field::parse(&date_char, pack);break;
+                case 'D':state += day_field::parse(&date_char, pack);break;
+                case 'h':state += hour_field::parse(&date_char, pack);break;
+                case 'm':state += minute_field::parse(&date_char, pack);break;
+                case 's':state += second_field::parse(&date_char, pack);break;
+                case 'z':state += microsecond_field::parse(&date_char, pack);break;
+                default:state += separator_field<1>::parse(&date_char, pack);break;
                 }
             }
-            return seconds_since_epoch(day, month, year, hour, minute,second) * 1000000LL + microsecond;
+            return datetime(pack.day, pack.month, pack.year, pack.hour, pack.minute, pack.second, pack.microsecond).data;
         }
         return parse_datetime_string(date, "YYYY-MM-DDThh:mm:ss+00:00", date_format::text_date);
     }
@@ -431,11 +172,9 @@ namespace gtr {
     }
 
     bool
-    datetime::to_string_format(char* out, char* error,const char* format, date_format group_format) const
+    datetime::to_string_format(char* out,const char* format, date_format group_format) const
     {
-        datetime_pack pack;
-        to_pack(pack);
-        return datetime_to_string(pack.year, pack.month, pack.day, pack.hour, pack.minute, pack.second, pack.microsecond, out,error, format, group_format);
+        return datetime_to_string(*this,out, format, group_format);
     }
     
     void
--- datetime.h
+++ datetime.h
@@ -2,13 +2,68 @@
 #define GTR_DATETIME_H
 constexpr auto DATETIME_DEFAULT_FORMAT = "DD/MM/YYYY hh:mm:ss";
 constexpr auto DATETIME_INVALID = -9223372036854775807LL - 1LL;
+#define _IS_ALPHA(A) ((((A) >= '0') && ((A) <= '9')))
+#define _END_STRING(A) ((*(A)) = '\0')
 
 namespace gtr{
+    constexpr const char *datetime_month_abbrev[]{"Jan", "Feb", "Mar", "Apr",
+                                                  "May", "Jun", "Jul", "Aug",
+                                                  "Sep", "Oct", "Nov", "Dec"};
+
+    static inline int
+    datetime_get_month_from_sum(int sum){
+        static const int char_sum[] = {281,269,288,291,295,301,299,401,296,294,307,268};
+        int month = 1;
+        while(char_sum[month - 1] != sum) month++;
+        return month;
+    }
+
+    static inline void
+    datetime_strcpy(char* dest, const char* source){do{*dest++ = *source++;}while(*source != '\0');}
+
+    static inline long long
+    datetime_atoi(const char* buffer){
+        bool neg = false;
+        if (buffer[0] == '-'){
+            neg = true;
+            buffer++;
+        }
+        long long value = 0;
+        while(*buffer != '\0' && _IS_ALPHA(*buffer)) value = value * 10 + *buffer++ - '0';
+        return neg ? -value : value;
+    }
+
+    inline constexpr int
+    datetime_digits(int number){
+        if (number < 0) number = -number;
+        if (number < 10) return 1;
+        if (number < 100) return 2;
+        if (number < 1000) return 3;
+        if (number < 10000) return 4;
+        if (number < 100000) return 5;
+        if (number < 1000000) return 6;
+        return -1;
+    }
+
+    static inline void
+    datetime_puts_integer(char* dest, int digits, int number){
+        int neg = number < 0;
+        *(dest+= digits + neg) = '\0';
+        number = neg ? -number : number;
+        while(digits--) {*--dest = (number % 10) + '0'; number /= 10;}
+        if (neg) *--dest =  '-';
+    }
+
     enum class date_format{
         text_date,
         iso_date
     };
 
+    enum class month_format{
+        month_digits,
+        month_abbrev
+    };
+    
     struct datetime_pack{
         int year;
         unsigned char month;
@@ -41,7 +96,7 @@ namespace gtr{
         is_valid() const {return data != DATETIME_INVALID;} 
         
         bool
-        to_string_format(char* out, char* error,const char* format = DATETIME_DEFAULT_FORMAT, date_format group_format = date_format::text_date) const;
+        to_string_format(char* out,const char* format = DATETIME_DEFAULT_FORMAT, date_format group_format = date_format::text_date) const;
 
         bool
         from_string(const char* date, const char* format = DATETIME_DEFAULT_FORMAT, date_format group_format = date_format::text_date);
@@ -102,6 +157,187 @@ namespace gtr{
 
         inline datetime
         operator-=(datetime other) {data -= other.data; return *this; }
-    };  
+        
+    };
+    
+    struct year_field{
+        static inline int parse(const char** state, datetime_pack& pack){
+            char buffer[8];
+            int index = 0;
+            while(_IS_ALPHA(**state)){buffer[index++] = *(*state)++;}
+            buffer[index] = '\0';
+            pack.year = datetime_atoi(buffer);
+            return index;
+        }
+
+        static inline void puts(const char** format,char** out,datetime_pack& pack){
+            if (*(++*format) == 'Y'){
+                // Four digits
+                if (*(++*format) == 'Y'){
+                    ++*format;
+                    datetime_puts_integer(*out, 4, pack.year);
+                    *out+= 4  + (pack.year < 0);
+                }
+                //Two digits
+                else{
+                    int digits = datetime_digits(pack.year);
+                    int year = pack.year;
+                    int factor = 1;
+                    for(;digits - 2 > 0;digits--){factor*=10;}
+                    datetime_puts_integer(*out, 2,year % factor);
+                    *out+= 2 + (pack.year < 0);
+
+                }
+            }
+            // All digits
+            else if (**format == 'F'){
+                int digits = datetime_digits(pack.year);
+                datetime_puts_integer(*out,digits, pack.year);
+                *out+= digits + (pack.year < 0);
+                (*format)++;
+            }
+        }
+    };
+    struct day_field{
+        static inline int parse(const char** state, datetime_pack& pack){
+            char buffer[8];          
+            buffer[0] = *(*state)++;
+            buffer[1] = *(*state)++;
+            _END_STRING(&buffer[2]); 
+            pack.day = datetime_atoi(buffer);
+            return 2;
+        }
+        static inline void puts(const char** format, char** out, datetime_pack& pack){
+            *format+=2;
+            datetime_puts_integer(*out, 2, pack.day);
+            *out+=2;
+        }
+    };
+
+    template<month_format Format = month_format::month_digits>struct month_field{
+        static inline int parse(const char** state, datetime_pack& pack){
+            if constexpr(Format == month_format::month_digits){
+                char buffer[8];          
+                buffer[0] = *(*state)++;
+                buffer[1] = *(*state)++;
+                _END_STRING(&buffer[2]); 
+                pack.month = datetime_atoi(buffer);
+                return 2;
+            }
+            else{
+                pack.month = datetime_get_month_from_sum((*state)[0] + (*state)[1] + (*state)[2]);
+                (*state)+=3;
+                return 3;
+            }
+        }
+        static inline void puts(const char** format, char** out, datetime_pack& pack){
+            if constexpr(Format == month_format::month_abbrev){
+                datetime_strcpy(*out, datetime_month_abbrev[pack.month - 1]);
+                *format+=3;
+                *out+=3;
+            }
+            else{
+                datetime_puts_integer(*out, 2, pack.month);
+                *format+=2;
+                *out+=2;
+            }
+        }
+        
+    };
+
+    struct hour_field{
+        static inline int parse(const char** state, datetime_pack& pack){   
+            char buffer[8];          
+            buffer[0] = *(*state)++;
+            buffer[1] = *(*state)++;
+            _END_STRING(&buffer[2]);     
+            pack.hour = datetime_atoi(buffer);
+            return 2;
+        }
+        static inline void puts(const char** format, char** out, datetime_pack& pack){
+            *format+=2;
+            datetime_puts_integer(*out, 2, pack.hour);
+            *out+=2;
+        }
+    };
+    struct minute_field{
+        static inline int parse(const char** state, datetime_pack& pack){
+            char buffer[8];          
+            buffer[0] = *(*state)++;
+            buffer[1] = *(*state)++;
+            _END_STRING(&buffer[2]);
+            pack.minute = datetime_atoi(buffer);
+            return 2;
+        }
+        static inline void puts(const char** format, char** out, datetime_pack& pack){
+            *format+=2;
+            datetime_puts_integer(*out, 2, pack.minute);
+            *out += 2;
+        }
+    };
+    
+    struct second_field{
+        static inline int parse(const char** state, datetime_pack& pack){
+            char buffer[8];          
+            buffer[0] = *(*state)++;
+            buffer[1] = *(*state)++;
+            _END_STRING(&buffer[2]);
+            pack.second = datetime_atoi(buffer);
+            return 2;
+        }
+        static inline void puts(const char** format, char** out, datetime_pack& pack){
+            *format += 2;
+            datetime_puts_integer(*out, 2, pack.second);
+            *out += 2;
+        }
+    };
+    
+    struct microsecond_field{
+        static inline int parse(const char **state, datetime_pack &pack) {
+            char buffer[8];
+            int index = 0;
+            while(_IS_ALPHA(**state)){buffer[index++] = *(*state)++;}
+            _END_STRING(&buffer[index]);
+            pack.microsecond = datetime_atoi(buffer);
+            for(int i = 0; i < 6 - index; i++){pack.microsecond*=10;};
+            return index;
+        }
+        static inline void puts(const char** format, char** out, datetime_pack& pack){
+            int digits = 1;
+            while(*(++*format) == 'z') {digits++;}
+            datetime_puts_integer(*out, digits, pack.microsecond);
+            *out += digits;
+        }
+    };
+
+    template<int Count = 1> struct separator_field{
+        static inline int parse(const char** state, datetime_pack& pack)
+        {
+            (void)pack;
+            (*state)+=Count;
+            return Count;
+        }
+        static inline void puts(const char** format, char** out, datetime_pack& pack){
+            (void)pack;
+            *(*out)++ = *(*format)++;
+        }
+    };
+#ifdef DATETIME_PERFECT_PARSER
+    template<class...Args>
+    struct perfect_parser{
+        static datetime parse_datetime(const char* date)
+        {
+            datetime_pack pack;
+            const char* state = date;
+            parse_impl(&state, pack);
+            return datetime{pack.day, pack.month, pack.year, pack.hour, pack.minute, pack.second, pack.microsecond};           
+        }
+        private:
+        static void parse_impl(const char** state, datetime_pack& pack)
+        {
+            ((void)Args{}.parse(state, pack), ...);
+        }
+    };
+#endif //DATETIME_PERFERCT_PARSER
 };
 #endif
